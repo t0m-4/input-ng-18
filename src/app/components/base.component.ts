1c9f08b0c9d46f95c8ecda5aaac814479c00be33
Thomas Siefer <t.siefer@sirfull.com>
2024-07-03 21:33:17 +0200

    up

--- src/app/components/base.component.ts
+++ src/app/components/base.component.ts
@@ -1,7 +1,6 @@
 import { Component, Directive, Input, signal } from "@angular/core";
 
-@Directive()
-// @Component({ template: '' }) // Also not working
+@Component({ template: '' })
 export class BaseComponent {
     @Input() address: string = '';
     phone = signal<string>('');
